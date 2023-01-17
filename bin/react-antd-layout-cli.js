c9444c2457dfb7e2a37b1a2420ab04b43521d0dc
lishengqin <lishengqin@hzanchu.com>
2023-01-17 13:39:47 +0800

    feat:优化脚本提示

--- bin/react-antd-layout-cli.js
+++ bin/react-antd-layout-cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import path from 'path';
+import path, { resolve } from 'path';
 import fs from 'fs';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
@@ -55,7 +55,7 @@ function _request(url) {
         method: 'GET',
         headers: {
           Authorization:
-            'token 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',
+            'token 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',
           'user-agent': 'lishengqin-Octocat-reactLayoutCli',
         },
       },
@@ -89,8 +89,8 @@ async function deepGet(list) {
       .map(one => one.url)
       .map(one => _request(one));
     let _list = await Promise.all(PromiseAll);
-    for (let i = 0; i < _list.length; i++) {
-      await deepGet(_list[i]);
+    for (let _one of _list) {
+      await deepGet(_one);
     }
   } catch (e) {
     console.log(chalk.red(e));
@@ -105,6 +105,7 @@ async function getFolder() {
   await deepGet(folderLevel1);
 }
 let errorRequestCount = 0;
+let templateCreatSuccess = false;
 async function createProject() {
   try {
     /* 请求太慢，直接读取文件中的目录 */
@@ -113,20 +114,24 @@ async function createProject() {
     /* 请求文件内容 */
     console.log('正在请求 https://github.com/lishengqin/react-antd-admin 项目的模板内容...');
     await getFileContent();
-    console.log(chalk.green('\n项目创建成功！'));
-    console.log(`
-  cd ${projectName}
-  初始化：npm install
-  启动接口node服务：npm run service
-  启动项目服务：npm run dev`);
-  } catch (e) {}
+    if (templateCreatSuccess) {
+      console.log(chalk.green('\n项目创建成功！'));
+      console.log(`
+    cd ${projectName}
+    初始化：npm install
+    启动接口node服务：npm run service
+    启动项目服务：npm run dev`);
+    }
+  } catch (e) {
+    console.log();
+  }
 }
 async function getFileContent(list = templateFolders) {
   if (!list.length) {
     return;
   }
-  /* 重复请求超过3次就直接提示仓库克隆项目 */
-  if (errorRequestCount > 3 && list.length) {
+  /* 重复请求超过5次就直接提示仓库克隆项目 */
+  if (errorRequestCount > 5 && list.length) {
     console.log(chalk.red('github请求项目模板内容超时'));
     console.log(
       chalk.blue('可以直接克隆仓库项目，地址：https://github.com/lishengqin/react-antd-admin')
@@ -135,15 +140,14 @@ async function getFileContent(list = templateFolders) {
   }
   errorRequestCount++;
   let errorRequestFiles = [];
-  for (let i = 0; i < list.length; i++) {
-    let one = list[i];
+  for (let one of list) {
     if (one.type === 'file') {
       // 获取type为file的文件内容
       try {
+        let res = await _request(one.url);
         if (errorRequestCount > 1) {
           console.log(chalk.green(one.path + ' 请求该文件成功'));
         }
-        let res = await _request(one.url);
         res = JSON.parse(res);
         let content = base64ToString(res.content);
         if (one.name === 'package.json') {
@@ -165,7 +169,9 @@ async function getFileContent(list = templateFolders) {
   }
   /* 有些请求失败的文件再请求一下 */
   if (errorRequestFiles.length) {
-    getFileContent(errorRequestFiles);
+    await getFileContent(errorRequestFiles);
+  } else {
+    templateCreatSuccess = true;
   }
 }
 createProject();
